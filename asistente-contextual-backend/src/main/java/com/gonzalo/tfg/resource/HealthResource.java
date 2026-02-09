deaf4b5716fefdf6f2de12eb1cb963f98615a3d5
Gonzaloogm <146828065+Gonzaloogm@users.noreply.github.com>
2026-02-09 08:26:51 +0100

    Correction commit

--- asistente-contextual-backend/src/main/java/com/gonzalo/tfg/resource/HealthResource.java
+++ asistente-contextual-backend/src/main/java/com/gonzalo/tfg/resource/HealthResource.java
@@ -14,7 +14,7 @@ public class HealthResource {
         return new HealthResponse(
                 "OK",
                 "Asistente Contextual Backend",
-                "1.0.0-SNAPSHOT",
+                "1.0.0",
                 "Sistema operativo correctamente");
     }
 
